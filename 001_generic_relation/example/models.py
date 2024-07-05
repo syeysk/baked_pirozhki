1837133bdc0da685bdcab46c776ac6fe6ea02066
shyzik93 <shyzik93@mail.ru>
2024-07-05 19:39:28 +0300

    change style

--- 001_generic_relation/example/models.py
+++ 001_generic_relation/example/models.py
@@ -1,17 +1,33 @@
-from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
-from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import (
+    GenericForeignKey,
+    GenericRelation,
+)
+from django.contrib.contenttypes.models import (
+    ContentType
+)
 from django.db import models
 
 
 class YourFirst(models.Model):
-    mains = GenericRelation('example.YourMain', related_query_name='first', related_name='firsts')
+    mains = GenericRelation(
+        'example.YourMain',
+        related_query_name='first',
+        related_name='firsts',
+    )
 
 
 class YourSecond(models.Model):
-    mains = GenericRelation('example.YourMain', related_query_name='second', related_name='seconds')
+    mains = GenericRelation(
+        'example.YourMain',
+        related_query_name='second',
+        related_name='seconds',
+    )
 
 
 class YourMain(models.Model):
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    content_type = models.ForeignKey(
+        ContentType,
+        on_delete=models.CASCADE,
+    )
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey()
